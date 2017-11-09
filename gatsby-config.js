8cd7c74216e4dd469626212156ef6a813b9f21a5
Jacob Fowles <js.fowles@gmail.com>
2017-11-09 09:52:09 -0700

    added some stuff and now it works

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `enuudjgqptyk`,
-        accessToken: `f199cbb7753bf3c0798fd5d42fad3080f28a0be27d95121bb97925972ac8dcad`
+        accessToken: `23d0a47d8859e21c5c9978bf2953a079a75cb4c5b7cabdffeea48b09f12f5e0b`
       }
     },
     `gatsby-plugin-react-helmet`
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import * as PropTypes from "prop-types"
 import Link from 'gatsby-link'
 
-export default class SecondPage extends React.Component {
+const propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+class SecondPage extends React.Component {
   render() {
+    const postEdges = this.props.data.id.edges
+    {console.log(postEdges[0].node.title)}
     return (
     <div>
-      <h1>Hye</h1>
-      <p>Welcome to page 2</p>
+      <h1>Title: {postEdges[0].node.title}</h1>
+      <h1>Description: {postEdges[0].node.description}</h1>
+      <h1>Number: {postEdges[0].node.number}</h1>
       <Link to="/">Go back to the homepage</Link>
     </div>)
   }
 }
+
+SecondPage.propTypes = propTypes
+
+export default SecondPage
+
+export const pageQuery = graphql`
+  query PageQuery {
+    id: allContentfulPost {
+      edges {
+        node {
+          id
+          title
+          description
+          number
+        }
+      }
+    }
+  }
+`
